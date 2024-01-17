811c547c4d388e82828655c2986630f30b845a99
Maxim <lyskomax2@mail.ru>
2024-01-17 16:05:23 +0300

    Add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задание 2.Задайте массив из N случайных целых чисел (N вводится с клавиатуры). Найдите количество чисел, которые оканчиваются на 1 и делятся нацело на 7.
+
+using System.ComponentModel;
+
+int[] CreateArray(int size)
+{
+    Random rnd = new Random();
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(0, 100);
+    }
+    return arr;
+}
+
+Console.WriteLine("Введите число");
+int number = Int32.Parse(Console.ReadLine());
+int[] arr = CreateArray(number);
+
+Console.WriteLine("Массив:");
+foreach (int e in arr)
+{
+    Console.Write(e + " ");
+}
+
+int count = 0;
+foreach (int e in arr)
+{
+    if (e > 9)
+    {
+        if (e % 10 == 1 && e % 7 == 0)
+            {
+                count++;
+            }
+    }
+}
+Console.Write($"\nКоличество чисел - {count}");
